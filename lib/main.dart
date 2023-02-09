25c24506050ac654cc1c67eb7b5989ce970e4a6f
dagmawitmoges <dagimd900@gmail.com>
2023-02-09 08:19:24 +0300

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  // This widget is the root of the application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'dental clinic',
       home: const MyHomePage(title: 'logo'),
     );
